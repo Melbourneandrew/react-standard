ec3522319d11456e599c74b7557435aaec8f75ca
Zaydek Michels-Gualtieri <zaydek@8090.inc>
2025-12-15 19:42:05 -0800

    add documentation link

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,5 @@
+// https://nextjs.org/docs/app/api-reference/config/next-config-js/reactCompiler
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = { experimental: { reactCompiler: true } };
